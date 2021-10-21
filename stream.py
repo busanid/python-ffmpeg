222bd06c53983c91455ab3e9926d64315e88671d
Budi Santoso <budi@busanid.dev>
2021-10-21 15:18:44 +0000

    Adding python and bash shell

--- stream.py
+++ stream.py
@@ -21,27 +21,7 @@ for event in notifier.event_gen():
 
             try:
                 # p = subprocess.Popen(f"ffmpeg -re -i {INPUT_FILE_NAME} -c copy -f flv rtmp://157.245.197.168/live/{STREAM_NAME}")
-                p = subprocess.Popen(["ffmpeg", 
-                                        "-re",
-                                        "-i", INPUT_FILE_NAME,
-                                        "-acodec", "libmp3lame",
-                                        "-ar", "44100", 
-                                        "-b:a", "128k", 
-                                        "-pix_fmt", "yuv420p", 
-                                        "-profile:v", "baseline", 
-                                        "-s", "426x240", 
-                                        "-bufsize", "6000k", 
-                                        "-vb", "400k", 
-                                        "-maxrate", "1500k", 
-                                        # "-deinterlace", 
-                                        "-vcodec", "libx264", 
-                                        "-preset", "veryfast", 
-                                        "-g", "30", 
-                                        "-r", "30", 
-                                        "-f", "flv", 
-                                        "-flvflags", "no_duration_filesize", 
-                                        stream_url,
-                                        ],
+                p = subprocess.Popen(["ffmpeg -re -i {INPUT_FILE_NAME} -c copy -f flv {stream_url}"],
                                         shell=True,
                                         stdout=subprocess.PIPE,
                                         stderr=subprocess.STDOUT
--- streaming3.py
+++ streaming3.py
@@ -1,6 +1,5 @@
 import inotify.adapters
 import os
-import uuid
 
 notifier = inotify.adapters.Inotify()
 notifier.add_watch('/data/streaming')
@@ -10,11 +9,6 @@ for event in notifier.event_gen():
         # Print event # Uncomment to see all events generated
         if 'IN_CREATE' in event[1]:
             INPUT_FILE_NAME = event[3]
-            STREAM_NAME = str(uuid.uuid4())
-            # print("file {} created in {}".format(event[3], event[2])
-            url = f"rtmp://157.245.197.168/live/{STREAM_NAME}"
-
-            with open ("url.txt", "a") as f:
-                f.write(url)
-
+            STREAM_NAME = event[3]
+            #print("file '{0}' created in '{1}'".format(event[3], event[2])
             os.system(f"ffmpeg -re -i {INPUT_FILE_NAME} -c copy -f flv rtmp://157.245.197.168/live/{STREAM_NAME}")
